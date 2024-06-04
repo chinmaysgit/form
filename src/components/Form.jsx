cbc0550cecff14e4e280cffbe712cd8d7a5439f3
Chinmay <sahuchinmaykumar248@gmail.com>
2024-06-04 11:40:35 +0530

    All bugs resolved

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Form() {
   const [name, setName] = useState("");
@@ -7,7 +7,9 @@ function Form() {
   const [desc, setDesc] = useState("");
   const [disable, setDisable] = useState(false);
   const [msg, setMsg] = useState("");
+  const [eye, setEye] = useState("fa-solid fa-eye");
   const showAsObj = {};
+  
   function handleSubmit(e) {
     e.preventDefault();
     if (name && email && password) {
@@ -19,41 +21,58 @@ function Form() {
       console.log(showAsObj);
     }
   }
-  function checkDisable(){
-    if (name && email && password){
-        setDisable(true);
+
+  useEffect(() => {
+    if (name && email && password) {
+      setDisable(true);
+
+    }else{
+      setDisable(false);
+    }
+  },[name,email,password])
+  function changeEye(e){
+    if(e.target.parentElement.children[0].type == "password"){
+      e.target.parentElement.children[0].type = "text";
+      setEye("fa-solid fa-eye-slash")
+    }else{
+      e.target.parentElement.children[0].type = "password";
+      setEye("fa-solid fa-eye")
     }
   }
   return (
     <div className="container">
-        <h2 style={{color: "#fff"}}>{msg}</h2>
+      <h2 style={{ color: "#fff" }}>{msg}</h2>
       <form onSubmit={(e) => handleSubmit(e)}>
         <input
           type="text"
           placeholder="CapFront"
-          onChange={(e) => {setName(e.target.value);
-            checkDisable();
-          }} value={name}
-        />
-        <input
-          type="text"
-          placeholder="example@gmail.com"
-          onChange={(e) => {setEmail(e.target.value);
-            checkDisable();
-          }
-        } value={email}
-        />
-        <input
-          type="text"
-          placeholder="password@1"
           onChange={(e) => {
-            setPassword(e.target.value);
-            checkDisable();
-
-          }} value={password}
+            setName(e.target.value);
+          }}
+          value={name}
         />
+        <input
+          type="email"
+          placeholder="example@gmail.com"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          value={email}
+        />
+        <div className="password-field">
+          <input
+            type="password"
+            placeholder="password#1"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            value={password}
+          />
+          <i className={eye} onClick={(e) => changeEye(e)}></i>
+        </div>
+
         <p style={{ color: "red" }}>
-          {password.length > 0 &&
+          {password &&
             password.length < 6 &&
             "more than 6 char required"}
         </p>
@@ -61,7 +80,8 @@ function Form() {
           cols={10}
           rows={5}
           onChange={(e) => setDesc(e.target.value)}
-          placeholder="something about you" value={desc}
+          placeholder="something about you"
+          value={desc}
         ></textarea>
         <button
           style={{
@@ -73,14 +93,18 @@ function Form() {
         >
           Submit
         </button>
-        <button onClick={() => {
+        <button
+          onClick={() => {
             setName("");
             setEmail("");
             setPassword("");
             setDesc("");
             setDisable(false);
             setMsg("");
-        }}>Clear</button>
+          }}
+        >
+          Clear
+        </button>
       </form>
     </div>
   );
